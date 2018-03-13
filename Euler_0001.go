2dce5ecd9f8f52db24c407cdca5f29ff9b1740f0
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-13 18:50:13 -0500

    ADD: Euler 0001
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
    The sum of these multiples is 23.
    Find the sum of all the multiples of 3 or 5 below 1000.

---
+++ Euler_0001.go
@@ -0,0 +1,24 @@
+/// <summary>
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+/// The sum of these multiples is 23.
+/// Find the sum of all the multiples of 3 or 5 below 1000.
+/// </summary>
+package main
+import "fmt"
+// Global variables
+const  (FinalNumber = 1000
+		FirstFactor = 3
+		SecondFactor = 5)
+
+func main() {
+	var (
+	sum = 0
+	i = 0)
+	for i < FinalNumber {
+		if (i%FirstFactor == 0 && i%SecondFactor == 0) {
+			sum += i
+		}
+		i++
+	}
+	fmt.Println("The sum of all the natural numbers below", FinalNumber, "that are multiples of", FirstFactor, "and", SecondFactor, "is", sum)
+}
